c97ee66b95c71680e79108fb9ea6285fa9ada799
Timoteo Malves <timoteo.alves14@gmail.com>
2025-10-15 22:30:21 -0300

    (fix): ajusta update stock de produtos que não existem e responsabilidade da controller

--- product-service/index.js
+++ product-service/index.js
@@ -6,42 +6,20 @@ import productController from "./src/controllers/productController.js";
 app.use(express.json());
 
 app.get("/products", async (req, res) => {
-  try {
-    console.log("fetchAll received");
-    const products = await productController.fetchAll();
-    res.json(products);
-  } catch (error) {
-    res.status(500).json({ message: "Error getting products." });
-  }
+  console.log("fetchAll received");
+  await productController.fetchAll(req, res);
 });
 
 // Get product by ID
 app.get("/products/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    console.log("Filter received");
-    console.log(`Id received: ${id}`);
-    const filteredProduct = await productController.filterProduct(id);
-
-    if (filteredProduct) {
-      res.json(filteredProduct);
-    } else {
-      res.status(404).json({ message: "Product not found." });
-    }
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching product." });
-  }
+  console.log("Filter received");
+  await productController.filterProduct(req, res);
 });
 
 // Create new product
 app.post("/products", async (req, res) => {
-  try {
-    console.log("register received");
-    const newProduct = await productController.registerProduct(req.body);
-    res.status(201).json(newProduct);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+  console.log("register received");
+  await productController.registerProduct(req.body, res);
 });
 
 // Update product by ID
@@ -56,54 +34,23 @@ app.put("/products/:id", async (req, res) => {
     return;
   }
 
-  try {
-    console.log("edit received");
-    const { id } = req.params;
-    const updatedProduct = await productController.editProduct(
-      parseInt(id),
-      req.body
-    );
-
-    if (updatedProduct) {
-      res.json(updatedProduct);
-    } else {
-      res.status(404).json({ message: "Product not found." });
-    }
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+  console.log("edit received");
+  const { id } = req.params;
+  await productController.editProduct(parseInt(id), req.body, res);
 });
 
 // Update stock
 app.patch("/products/:id/stock", async (req, res) => {
-  try {
-    console.log("edit received");
-    const { id } = req.params;
-    const updatedProduct = await productController.editProduct(
-      parseInt(id),
-      req.body
-    );
-
-    if (updatedProduct) {
-      res.json(updatedProduct);
-    } else {
-      res.status(404).json({ message: "Product not found." });
-    }
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+  console.log("edit received");
+  const { id } = req.params;
+  await productController.editProduct(parseInt(id), req.body, res);
 });
 
 // Delete product by ID
 app.delete("/products/:id", async (req, res) => {
-  try {
-    console.log("delete received");
-    const { id } = req.params;
-    await productController.deleteProduct(parseInt(id));
-    res.status(200).json({ message: `Product deleted` });
-  } catch (error) {
-    res.status(500).json({ message: `Error deleting the product` });
-  }
+  console.log("delete received");
+  const { id } = req.params;
+  await productController.deleteProduct(parseInt(id), res);
 });
 
 app.listen(PORT, () => {
--- product-service/src/controllers/productController.js
+++ product-service/src/controllers/productController.js
@@ -2,27 +2,60 @@ import productRepository from "../repository/productRepository.js";
 
 class ProductController {
   // GET /products
-  async fetchAll() {
-    return productRepository.fetchAll();
+  async fetchAll(req, res) {
+    try {
+      const products = await productRepository.fetchAll();
+      res.status(200).json(products);
+    } catch (error) {
+      res.status(406).json({ message: error });
+    }
   }
 
   // GET /products/:id
-  async filterProduct(id) {
-    return productRepository.getById(id);
+  async filterProduct(req, res) {
+    const { id } = req.params;
+    console.log(`Id received: ${id}`);
+    try {
+      const filteredProduct = await productRepository.getById(id);
+
+      if (filteredProduct) {
+        res.status(200).json(filteredProduct);
+      } else {
+        res.status(406).json({ message: `No product with id ${id} was found` });
+      }
+    } catch (error) {
+      res.status(406).json({ message: error });
+    }
   }
 
   // POST /products
-  async registerProduct(productData) {
-    return productRepository.create(productData);
+  async registerProduct(productData, res) {
+    try {
+      const newProduct = await productRepository.create(productData);
+      res.status(200).json(newProduct);
+    } catch (error) {
+      res.status(406).json({ message: error });
+    }
   }
 
   // PUT /products/:id e PUT /products/:id/stock
-  async editProduct(id, updatedData) {
-    return productRepository.update(id, updatedData);
+  async editProduct(id, updatedData, res) {
+    try {
+      const updatedProduct = await productRepository.update(id, updatedData);
+      res.status(200).json(updatedProduct);
+    } catch (error) {
+      res.status(406).json({ message: error });
+    }
   }
 
   // DELETE /products/:id
-  async deleteProduct(id) {
+  async deleteProduct(id, res) {
+    try {
+      await productRepository.remove(id);
+      res.status(203).json({ message: `Product ${id} deleted` });
+    } catch (error) {
+      res.status(406).json({ message: error });
+    }
     return productRepository.remove(id);
   }
 }

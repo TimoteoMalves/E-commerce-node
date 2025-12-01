3c6f441389c4bc41f5aa57c65cd1bb666de23b60
Timoteo Malves <timoteo.alves14@gmail.com>
2025-12-01 18:53:05 -0300

    finaliza

--- rs_init.sh
+++ rs_init.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
+# rs-init.sh
 
-# Este script executa a inicialização do Replica Set (rs.initiate())
-# Ele roda automaticamente no startup do container MongoDB
+echo "=> Aguardando o orders-db iniciar..."
+
+sleep 15 
 
 echo "=> Iniciando a configuração do Replica Set 'rs0'..."
 
-# Acessa o shell do MongoDB e executa o comando rs.initiate
-# O sleep é para garantir que o mongod tenha iniciado completamente.
-mongosh --host orders-db:27017 <<EOF
-  rs.initiate({
-    _id: "rs0",
-    members: [
-      { _id: 0, host: "orders-db:27017" }
-    ]
-  }, { force: true });
-EOF
+mongosh --host orders-db:27017 --eval 'rs.initiate({_id:"rs0",members:[{_id:0,host:"orders-db:27017"}]}, {force:true});'
 
 echo "=> Configuração do Replica Set concluída."
